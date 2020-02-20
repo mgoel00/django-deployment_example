fc720de0708e9c69290dc646aceb6d6bc4964d04
Mayank Goel <goel.mayank.13@gmail.com>
2020-02-20 17:13:45 +0530

    changes setting.py

--- learning_users/learning_users/settings.py
+++ learning_users/learning_users/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '*h1f#*juczmtg&7wa3hex!3u5qo%wp0j(-e352n=n5zj+t4p93'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mayankg2000.pythonanywhere.com',]
 
 
 # Application definition
